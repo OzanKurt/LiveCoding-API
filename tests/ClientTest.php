1cf53c69c69c056c768b41a76b2f2cf3de2139a8
Ozan Kurt <me@ozankurt.com>
2016-01-15 23:05:10 +0100

    Initial files & Tests

---
+++ tests/ClientTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Kurt\LiveCoding\Client; 
+
+class ClientTest extends PHPUnit_Framework_TestCase
+{
+	protected $client;
+
+	public function setUp()
+	{
+		$this->client = new Client([
+		    'id' => '12631243816253178',
+		    'secret' => '123465129834629',
+		    'redirectUrl' => 'http://localhost:8000/index.php',
+		    'scope' => new Kurt\LiveCoding\Scopes\ReadScope,
+		]);
+	}
+
+	/** @test */
+	public function it_does_something()
+	{
+		$this->assertTrue(true);
+	}
+}
