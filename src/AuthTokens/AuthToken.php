028f6509a5969ab408db51f144b4ab67b7052666
Ozan Kurt <me@ozankurt.com>
2016-01-19 01:58:19 +0100

    Clean up

--- src/AuthTokens/AuthToken.php
+++ src/AuthTokens/AuthToken.php
@@ -3,25 +3,23 @@
 namespace Kurt\LiveCoding\AuthTokens;
 
 /**
- * @class AuthToken
- * AuthToken is intended to be semi-abstract
- * Only it's subclasses should be instantiated
+ * Class AuthToken
+ * @package Kurt\LiveCoding\AuthTokens
  */
 abstract class AuthToken
 {
+
     /**
      * Store token data to subclass defined backend.
-     **/
+     *
+     * @param $tokens
+     */
     public function storeTokens($tokens)
     {
         $tokens = json_decode($tokens);
 
         if (!isset($tokens->error)) {
-            $this->setAccessToken($tokens->accessToken);
-            $this->setTokenType($tokens->tokenType);
-            $this->setRefreshToken($tokens->refreshToken);
-            $this->setExpiresIn(date('Y-m-d H:i:s', (time() + $tokens->expiresIn)));
-            $this->setScope($tokens->scope);
+            $this->setVariables($tokens);
         }
     }
 
@@ -49,106 +47,146 @@ abstract class AuthToken
 
     /**
      * [isAuthorized description].
-     * 
+     *
      * @return bool [description]
      */
     abstract public function isAuthorized();
 
     /**
      * [getCode description].
-     * 
+     *
      * @return [type] [description]
      */
     abstract public function getCode();
 
     /**
      * [setCode description].
-     * 
+     *
      * @param [type] $code [description]
      */
     abstract public function setCode($code);
 
     /**
      * [getState description].
-     * 
+     *
      * @return [type] [description]
      */
     abstract public function getState();
 
     /**
      * [setState description].
-     * 
-     * @return [type] [description]
+     *
+     * @param $state
+     *
+     * @return  [type] [description]
      */
     abstract public function setState($state);
 
     /**
      * [getAccessToken description].
-     * 
+     *
      * @return [type] [description]
      */
     abstract public function getAccessToken();
 
     /**
      * [setAccessToken description].
-     * 
-     * @return [type] [description]
+     *
+     * @param $accessToken
+     *
+     * @return  [type] [description]
      */
     abstract public function setAccessToken($accessToken);
 
     /**
      * [getTokenType description].
-     * 
+     *
      * @return [type] [description]
      */
     abstract public function getTokenType();
 
     /**
      * [setTokenType description].
-     * 
-     * @return [type] [description]
+     *
+     * @param $tokenType
+     *
+     * @return  [type] [description]
      */
     abstract public function setTokenType($tokenType);
 
     /**
      * [getRefreshToken description].
-     * 
+     *
      * @return [type] [description]
      */
     abstract public function getRefreshToken();
 
     /**
      * [setRefreshToken description].
-     * 
-     * @return [type] [description]
+     *
+     * @param $refreshToken
+     *
+     * @return  [type] [description]
      */
     abstract public function setRefreshToken($refreshToken);
 
     /**
      * [getExpiresIn description].
-     * 
+     *
      * @return [type] [description]
      */
     abstract public function getExpiresIn();
 
     /**
      * [setExpiresIn description].
-     * 
-     * @return [type] [description]
+     *
+     * @param $expiresIn
+     *
+     * @return  [type] [description]
      */
     abstract public function setExpiresIn($expiresIn);
 
     /**
      * [getScope description].
-     * 
+     *
      * @return [type] [description]
      */
     abstract public function getScope();
 
     /**
      * [setScope description].
-     * 
-     * @return [type] [description]
+     *
+     * @param $scope
+     *
+     * @return  [type] [description]
      */
     abstract public function setScope($scope);
+
+    /**
+     * Set the variables from tokens.
+     *
+     * @param $tokens
+     */
+    protected function setVariables($tokens)
+    {
+        $this->setAccessToken(
+            $tokens->access_token
+        );
+
+        $this->setExpiresIn(
+            date('Y-m-d H:i:s', (time() + $tokens->expires_in))
+        );
+
+        $this->setRefreshToken(
+            $tokens->refresh_token
+        );
+
+        $this->setScope(
+            $tokens->scope
+        );
+
+        $this->setTokenType(
+            $tokens->token_type
+        );
+    }
 }
